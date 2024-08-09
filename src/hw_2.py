9908e13df4dff7570891f694dd90e6270dbd3336
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-08-08 19:44:56 -0500

    add p2()

--- src/hw_2.py
+++ src/hw_2.py
@@ -10,3 +10,16 @@ def p1():
 
     # python removes leading zeroes from int, return as string
     return rand_int
+
+def p2(guess: str, code: str):
+    if ((len(guess) != CODE_LENGTH) or (len(code) != CODE_LENGTH)):
+        raise ValueError("Incorrect code length")
+    
+    score = 0
+    for pos in range(0, CODE_LENGTH):
+        if guess[pos] == code[pos]:
+            score += 10
+        else:
+            score += 1
+        
+    return score
