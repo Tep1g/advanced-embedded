0c082d9e5ead8e5eb8647c701395cd27e3de8e66
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-08-08 19:41:06 -0500

    add global constants

--- src/hw_2.py
+++ src/hw_2.py
@@ -1,9 +1,12 @@
 from random import randrange
 
+CODE_LENGTH = 4
+MAX_DIGIT_VALUE = 6
+
 def p1():
     rand_int = ""
-    for _ in range(0,4):
-        rand_int += str(randrange(6))
+    for _ in range(0, CODE_LENGTH):
+        rand_int += str(randrange(MAX_DIGIT_VALUE))
 
     # python removes leading zeroes from int, return as string
     return rand_int
