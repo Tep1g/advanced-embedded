5ef6da2a5850cd2fc242d1e82c37e5c49e410a67
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-08-10 17:29:05 -0500

    separate p1() and p2()

--- src/hw_3.py
+++ src/hw_3.py
@@ -1,10 +1,23 @@
 from machine import Pin
 
-def hw_3():
+def p1():
+    button1 = Pin("GP15", Pin.IN)
+    button2 = Pin("GP14", Pin.IN)
+    led1 = Pin("GP16", Pin.OUT)
+    led2 = Pin("GP17", Pin.OUT)
 
-    class hw_stuff():
+    try:
+        while True:
+            led1.value((1 - button1.value()) & (1 - button2.value()))
+            led2.value((1 - button1.value()) ^ (1 - button2.value()))
+    except KeyboardInterrupt:
+        pass
+
+def p2():
+
+    class p2_stuff():
         def __init__(self):
-        
+
             self.button1 = Pin("GP15", Pin.IN)
             self.button1.irq(trigger=Pin.IRQ_RISING, handler=self._p2_callback1)
             self.button2 = Pin("GP14", Pin.IN)
@@ -19,11 +32,7 @@ def hw_3():
         def _p2_callback2(self, pin):
             self.counter += 1
 
-    def p1(stuff: hw_stuff):
-        stuff.led1.value((1 - stuff.button1.value()) & (1 - stuff.button2.value()))
-        stuff.led2.value((1 - stuff.button1.value()) ^ (1 - stuff.button2.value()))
-
-    stuff = hw_stuff()
+    stuff = p2_stuff()
 
     try:
         while True:
