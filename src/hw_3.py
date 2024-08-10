532c8933fdbd6378c9dda485ee9a1e61f853723e
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-08-10 13:11:28 -0500

    move variables to class object and add p2 callback functions

--- src/hw_3.py
+++ src/hw_3.py
@@ -2,22 +2,31 @@ from machine import Pin
 
 def hw_3():
 
-    def init():
-        button1 = Pin("GP15", Pin.IN)
-        button2 = Pin("GP14", Pin.IN)
-        led1 = Pin("GP16", Pin.OUT)
-        led2 = Pin("GP17", Pin.OUT)
+    class hw_stuff():
+        def __init__(self):
+        
+            self.button1 = Pin("GP15", Pin.IN)
+            self.button1.irq(trigger=Pin.IRQ_RISING, handler=self._p2_callback1)
+            self.button2 = Pin("GP14", Pin.IN)
+            self.button2.irq(trigger=Pin.IRQ_RISING, handler=self._p2_callback2)
+            self.led1 = Pin("GP16", Pin.OUT)
+            self.led2 = Pin("GP17", Pin.OUT)
+            self.counter = 0
+    
+        def _p2_callback1(self, pin):
+            self.counter += 10
 
-        return (button1, button2, led1, led2)
+        def _p2_callback2(self, pin):
+            self.counter += 1
 
-    def p1():
-        led1.value((1 - button1.value()) & (1 - button2.value()))
-        led2.value((1 - button1.value()) ^ (1 - button2.value()))
+    def p1(stuff: hw_stuff):
+        stuff.led1.value((1 - stuff.button1.value()) & (1 - stuff.button2.value()))
+        stuff.led2.value((1 - stuff.button1.value()) ^ (1 - stuff.button2.value()))
 
-    (button1, button2, led1, led2) = init()
+    stuff = hw_stuff()
 
     try:
         while True:
-            p1()
+            print(stuff.counter)
     except KeyboardInterrupt:
-        print("Exited")
+        pass
