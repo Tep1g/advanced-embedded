bdc10fb641317d22d48edee88577352ab288a9b2
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-08-29 18:07:46 -0500

    add sunflower() function

--- src/hw_4.py
+++ src/hw_4.py
@@ -1,4 +1,5 @@
 from machine import Pin, ADC, PWM
+import time
 
 def trombone():
 
@@ -15,3 +16,37 @@ def trombone():
                 trombone.duty_u16(0)
     except:
         return "Exited Successfully"
+    
+def sunflower():
+
+    def read_voltage(adc: ADC):
+        return (adc.read_u16() * 3.3 / 65535)
+    
+    def update_servo_angle(servo: PWM, angle: int):
+        duty_cycle_ns = int(((angle/135) * 1_000_000) + 500_000)
+        servo.duty_ns(duty_cycle_ns)
+
+    light = ADC(2)
+    servo = PWM(Pin(28, Pin.OUT))
+    servo.freq(50)
+    angle = 135
+    last_voltage = read_voltage(light)
+    increase_angle = True
+    try:
+        while True:
+            if increase_angle:
+                angle += 3
+            else:
+                angle -= 3
+            angle = max(min(angle, 270), 0)
+            update_servo_angle(servo, angle)
+            time.sleep_ms(1)
+            new_voltage = read_voltage(light)
+            if new_voltage < last_voltage:
+                increase_angle = not increase_angle
+            last_voltage = new_voltage
+            print("Current voltage: {:.2f}, Current angle {}".format(new_voltage, angle))
+
+    except:
+        return "Exited Successfully"
+    
