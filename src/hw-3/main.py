10291d2b7f95fc4a72eb345c16617e4eab785910
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-09-06 16:03:23 -0500

    modularize ButtonCounter class

--- src/hw-3/main.py
+++ src/hw-3/main.py
@@ -15,32 +15,30 @@ def p1():
     except KeyboardInterrupt:
         return "Exited Sucessfully"
 
-def p2():
 
-    class p2_stuff():
-        def __init__(self):
+class ButtonCounter():
+    def __init__(self, button_pin: int, button_mode: int, counter_ptr: list[int], increment: int):
+        self._button = Pin(button_pin, Pin.IN, button_mode)
+        self._button.irq(trigger=Pin.IRQ_FALLING, handler=self._button_callback)
+        self._counter_ptr = counter_ptr
+        self._increment = increment
 
-            self.button1 = Pin(15, Pin.IN, Pin.PULL_UP)
-            self.button1.irq(trigger=Pin.IRQ_RISING, handler=self._p2_callback1)
-            self.button2 = Pin(14, Pin.IN, Pin.PULL_UP)
-            self.button2.irq(trigger=Pin.IRQ_RISING, handler=self._p2_callback2)
-            self.led1 = Pin(16, Pin.OUT)
-            self.led2 = Pin(17, Pin.OUT)
-            self.counter = 0
+    def _button_callback(self, pin: Pin):
+        self._counter_ptr[0] += self._increment
+
+def counter():
+    """Counter program"""
+    counter_ptr = [0]
     
-        def _p2_callback1(self, pin):
-            self.counter += 10
-
-        def _p2_callback2(self, pin):
-            self.counter += 1
-
-    stuff = p2_stuff()
+    ButtonCounter(15, Pin.PULL_UP, counter_ptr, 10)
+    ButtonCounter(14, Pin.PULL_UP, counter_ptr, 1)
 
     try:
         while True:
-            print(stuff.counter)
+            print(counter_ptr[0])
     except KeyboardInterrupt:
         return "Exited Sucessfully"
+
 
 class SN74165N():
     """
