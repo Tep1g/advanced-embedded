b42181ae489e9dc463c5d24f6d458fc7541f5e70
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-09-16 17:50:03 -0500

    fix duty cycle setting

--- src/hw-4/trombone/main.py
+++ src/hw-4/trombone/main.py
@@ -16,13 +16,15 @@ if __name__ == "__main__":
     play_btn = Pin(BUTTON_GPIO, Pin.IN, Pin.PULL_UP)
     joystick = ADC(ADC_PORT)
     trombone = PWM(Pin(BUZZER_GPIO, Pin.OUT))
-    trombone.duty_u16(ceil(MAX_U16 / 2))
+    trombone.duty_u16(0)
+    trombone_is_on = False
     
     while True:
         if play_btn.value() == 0:
             freq = int(((joystick.read_u16() / MAX_U16) * FREQ_RANGE) + MIN_FREQ)
             trombone.freq(freq)
-            currently_playing = True
-        elif currently_playing:
+            trombone.duty_u16(ceil(MAX_U16 / 2))
+            trombone_is_on = True
+        elif trombone_is_on:
             trombone.duty_u16(0)
-            currently_playing = False
+            trombone_is_on = False
