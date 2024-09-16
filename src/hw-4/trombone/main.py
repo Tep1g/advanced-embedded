e8b9ce501860aafbc762e5e5a5bcda101c47cad9
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-09-16 17:54:20 -0500

    don't redundantly turn on trombone

--- src/hw-4/trombone/main.py
+++ src/hw-4/trombone/main.py
@@ -23,8 +23,10 @@ if __name__ == "__main__":
         if play_btn.value() == 0:
             freq = int(((joystick.read_u16() / MAX_U16) * FREQ_RANGE) + MIN_FREQ)
             trombone.freq(freq)
-            trombone.duty_u16(ceil(MAX_U16 / 2))
-            trombone_is_on = True
+            if not trombone_is_on:
+                trombone.duty_u16(ceil(MAX_U16 / 2))
+                trombone_is_on = True
+        
         elif trombone_is_on:
             trombone.duty_u16(0)
             trombone_is_on = False
