fb42b7b6bd41f26eb543c6606f22b04923060a83
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-09-18 14:36:05 -0500

    add test script comment

--- src/hw-4/sunflower/sense.py
+++ src/hw-4/sunflower/sense.py
@@ -4,6 +4,7 @@ def read_voltage(adc: ADC):
     return (adc.read_u16() * 3.3 / 65535)
 
 if __name__ == "__main__":
+    """Test Script"""
     while True:
         light = ADC(2)
         print(read_voltage(light))
--- src/hw-4/sunflower/servo.py
+++ src/hw-4/sunflower/servo.py
@@ -5,6 +5,7 @@ def update_servo_angle(servo: PWM, angle: int):
     servo.duty_ns(duty_cycle_ns)
 
 if __name__ == "__main__":
+    """Test Script"""
     servo = PWM(Pin(28, Pin.OUT))
     while True:
         angle = int(input("Enter new servo angle: "))
