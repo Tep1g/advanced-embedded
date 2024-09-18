dde04972c7c14aaa6271c14d5aed73ca26781c0f
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-09-18 16:54:37 -0500

    encapsulate constants

--- src/hw-4/sunflower/sensor.py
+++ src/hw-4/sunflower/sensor.py
@@ -1,12 +1,13 @@
 from machine import ADC
 
-_VDD = 3.3
-_MAX_U16 = (2 ** 8) - 1
-
-_V_FACTOR = _VDD / _MAX_U16
-
 class LightSensor():
     """Light Sensor"""
+
+    _VDD = 3.3
+    _MAX_U16 = (2 ** 8) - 1
+
+    _V_FACTOR = _VDD / _MAX_U16
+
     def __init__(self, adc_port: int):
         """
         Initialize the LightSensor object
@@ -17,7 +18,7 @@ class LightSensor():
 
     def read_voltage(self) -> float:
         """Return the ADC voltage as a float"""
-        return (self._adc.read_u16() * _V_FACTOR)
+        return (self._adc.read_u16() * self._V_FACTOR)
 
 if __name__ == "__main__":
     """Test Script"""
--- src/hw-4/sunflower/servo.py
+++ src/hw-4/sunflower/servo.py
@@ -1,18 +1,24 @@
 from machine import Pin, PWM
 
-_MIN_FREQ_HZ = 50
-_MAX_FREQ_HZ = 330
-
-_MIN_PW_NS = 500_000
-_MAX_PW_NS = 2_500_000
-_MIN_ANGLE = 0
-_MAX_ANGLE = 270
-
-_PW_FACTOR = (_MAX_PW_NS - _MIN_PW_NS) / _MAX_ANGLE
-
 class ServoMotor():
     """Servo Motor"""
+
+    MIN_FREQ_HZ = 50
+    MAX_FREQ_HZ = 330
+
+    MIN_ANGLE = 0
+    MAX_ANGLE = 270
+    _MIN_PW_NS = 500_000
+    _MAX_PW_NS = 2_500_000
+
+    _PW_FACTOR = (_MAX_PW_NS - _MIN_PW_NS) / MAX_ANGLE
+
     def __init__(self, pwm_gpio: int):
+        """
+        Initialize the ServoMotor object
+        
+        :param int pwm_gpio: PWM GPIO pin
+        """
         self._pwm = PWM(Pin(pwm_gpio, Pin.OUT))
 
     def set_speed(self, freq_hz: int):
@@ -21,8 +27,8 @@ class ServoMotor():
         
         :param int freq_hz: Between 50Hz (slowest) and 330Hz (fastest)
         """
-        if not (_MIN_FREQ_HZ <= freq_hz <= _MAX_FREQ_HZ):
-            raise ValueError("Invalid frequency for servo speed control: {}, must be between {} and {} inclusive".format(freq_hz, _MIN_FREQ_HZ, _MAX_FREQ_HZ))
+        if not (self.MIN_FREQ_HZ <= freq_hz <= self.MAX_FREQ_HZ):
+            raise ValueError("Invalid frequency for servo speed control: {}, must be between {} and {} inclusive".format(freq_hz, self.MIN_FREQ_HZ, self.MAX_FREQ_HZ))
 
         self._pwm.freq(freq_hz)
 
@@ -32,11 +38,11 @@ class ServoMotor():
          
         :param int angle_deg: Must be between 0 and 270 degrees
         """
-        if not (_MIN_ANGLE <= angle_deg <= _MAX_ANGLE):
-            raise ValueError("Invalid servo angle: {}, must be between {} and {} inclusive".format(angle_deg, _MIN_ANGLE, _MAX_ANGLE))
+        if not (self.MIN_ANGLE <= angle_deg <= self.MAX_ANGLE):
+            raise ValueError("Invalid servo angle: {}, must be between {} and {} inclusive".format(angle_deg, self.MIN_ANGLE, self.MAX_ANGLE))
 
-        duty_cycle_ns = int((angle_deg) * _PW_FACTOR) + _MIN_PW_NS
-        duty_cycle_ns = max(min(duty_cycle_ns, _MAX_PW_NS), _MIN_PW_NS)
+        duty_cycle_ns = int((angle_deg) * self._PW_FACTOR) + self._MIN_PW_NS
+        duty_cycle_ns = max(min(duty_cycle_ns, self._MAX_PW_NS), self._MIN_PW_NS)
         self._pwm.duty_ns(duty_cycle_ns)
 
 if __name__ == "__main__":
