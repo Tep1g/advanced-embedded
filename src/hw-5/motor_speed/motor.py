eba95686ad2c32f1da0164ea38b8149f0767ef2c
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-09-26 14:42:25 -0500

    add bidrectional set_speed_pct()

--- src/hw-5/motor_speed/motor.py
+++ src/hw-5/motor_speed/motor.py
@@ -23,6 +23,20 @@ class MonodirectionalMotor():
         self._pwm.duty_ns(round(self._duty_ns * speed_pct))
 
 class BidirectionalMotor():
+    MIN_SPEED_PCT = const(-100)
+    MAX_SPEED_PCT = const(100)
+    
     def __init__(self, cw_gpio: int, ccw_gpio: int, freq_hz: int):
         self.cw = MonodirectionalMotor(pwm_gpio=cw_gpio, freq_hz=freq_hz)
         self.ccw = MonodirectionalMotor(pwm_gpio=ccw_gpio, freq_hz=freq_hz)
+
+    def set_speed_pct(self, speed_pct: float):
+        if not (self.MIN_SPEED_PCT <= speed_pct <= self.MAX_SPEED_PCT):
+            raise ValueError("Invalid bidirectional speed percentage: {}, must be within {} and {} inclusive.".format(speed_pct, self.MIN_SPEED_PCT, self.MAX_SPEED_PCT))
+
+        if speed_pct < 0:
+            self.cw.set_speed_pct(0)
+            self.ccw.set_speed_pct(abs(speed_pct))
+        else:
+            self.ccw.set_speed_pct(0)
+            self.cw.set_speed_pct(speed_pct)
