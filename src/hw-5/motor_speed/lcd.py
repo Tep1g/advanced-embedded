278401ec1652e16aab73752e9ac2b217ca072a22
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-03 17:25:51 -0500

    add lcd module to hw5

---
+++ src/hw-5/motor_speed/lcd.py
@@ -0,0 +1,15 @@
+from lib import st7796 #Professor Glower's st7796 driver, renamed for clarity
+
+RGB_BLACK = const(st7796.RGB(0,0,0))
+RGB_WHITE = const(st7796.RGB(255, 255, 255))
+GRAPH_TITLE = const("Motor Speed")
+POS_CHAR = const(" ")
+NEG_CHAR = const("-")
+
+def init():
+    st7796.Init()
+    st7796.Clear(RGB_BLACK)
+    st7796.Title(Message=GRAPH_TITLE, Color1=RGB_WHITE, Color0=RGB_BLACK)
+
+def update_motor_speed(value: int):
+    st7796.BarChart(X=[value], color1=RGB_WHITE, color2=RGB_BLACK)
