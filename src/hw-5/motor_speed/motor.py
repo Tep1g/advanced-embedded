7fb010ce4c9f41e6d64c4589ccd112f3b9dd57fd
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-03 21:39:21 -0500

    fix motor constants

--- src/hw-5/motor_speed/motor.py
+++ src/hw-5/motor_speed/motor.py
@@ -5,34 +5,34 @@ class UnidirectionalMotor():
 
     MIN_SPEED_PCT = const(0)
     MAX_SPEED_PCT = const(100)
-    _SEC_TO_NS_FLOAT = const(1_000_000_000)
-    _PWM_FREQ = const(20_000)
-    _PWM_DUTY_NS = const(_SEC_TO_NS_FLOAT / _PWM_FREQ)
+    SEC_TO_NS_FLOAT = const(1_000_000_000)
+    PWM_FREQ = const(20_000)
+    PWM_DUTY_NS = SEC_TO_NS_FLOAT / PWM_FREQ
     
     def __init__(self, pwm_gpio: int):
         self._pwm = PWM(Pin(pwm_gpio, Pin.OUT))
         self._pwm.duty_ns(0)
-        self._pwm.freq(self._PWM_FREQ)
-        self._duty_ns = round(self._SEC_TO_NS_FLOAT / self._PWM_FREQ)
+        self._pwm.freq(self.PWM_FREQ)
+        self._duty_ns = round(self.SEC_TO_NS_FLOAT / self.PWM_FREQ)
 
     def set_speed_pct(self, speed_pct: int):
         if not (self.MIN_SPEED_PCT <= speed_pct <= self.MAX_SPEED_PCT):
             raise ValueError("Invalid monodirectional speed percentage: {}, must be within {} and {} inclusive.".format(speed_pct, self.MIN_SPEED_PCT, self.MAX_SPEED_PCT))
         
-        self._pwm.duty_ns(self._PWM_DUTY_NS * speed_pct / 100)
+        self._pwm.duty_ns(round(self.PWM_DUTY_NS * speed_pct / self.MAX_SPEED_PCT))
 
 class BidirectionalMotor():
     """Bidirectional Motor"""
-    MIN_SPEED_PCT = const(-100)
-    MAX_SPEED_PCT = const(100)
+    MIN_BI_SPEED_PCT = const(-100)
+    MAX_BI_SPEED_PCT = const(100)
     
     def __init__(self, cw_gpio: int, ccw_gpio: int):
         self._cw = UnidirectionalMotor(pwm_gpio=cw_gpio)
         self._ccw = UnidirectionalMotor(pwm_gpio=ccw_gpio)
 
     def set_speed_pct(self, speed_pct: int):
-        if not (self.MIN_SPEED_PCT <= speed_pct <= self.MAX_SPEED_PCT):
-            raise ValueError("Invalid bidirectional speed percentage: {}, must be within {} and {} inclusive.".format(speed_pct, self.MIN_SPEED_PCT, self.MAX_SPEED_PCT))
+        if not (self.MIN_BI_SPEED_PCT <= speed_pct <= self.MAX_BI_SPEED_PCT):
+            raise ValueError("Invalid bidirectional speed percentage: {}, must be within {} and {} inclusive.".format(speed_pct, self.MIN_BI_SPEED_PCT, self.MAX_BI_SPEED_PCT))
 
         if speed_pct < 0:
             self._cw.set_speed_pct(0)
@@ -44,4 +44,4 @@ class BidirectionalMotor():
 if __name__ == "__main__":
     motor = BidirectionalMotor(cw_gpio=16, ccw_gpio=17)
     while (True):
-        motor.set_speed_pct(int(input("Set DC servo motor speed percentage: ".format(motor.MIN_SPEED_PCT, motor.MAX_SPEED_PCT))))
+        motor.set_speed_pct(int(input("Set DC servo motor speed percentage: ".format(motor.MIN_BI_SPEED_PCT, motor.MAX_BI_SPEED_PCT))))
