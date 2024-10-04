7ac772498ec0b70c0b0c8ccce7a3f9d7976a8fc6
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-04 03:15:03 -0500

    fix display bug

--- src/hw-5/motor_speed/lcd.py
+++ src/hw-5/motor_speed/lcd.py
@@ -1,17 +1,18 @@
 import st7796 #Professor Glower's st7796 driver, renamed for clarity
+from motor import BidirectionalMotor
 
-RGB_BLACK = (0,0,0)
-RGB_WHITE = (255, 255, 255)
-GRAPHIC_BAR_X1 = const(220)
-GRAPHIC_BAR_X2 = const(260)
-GRAPHIC_BAR_Y1 = const(160)
-POS_CHAR = const(" ")
-NEG_CHAR = const("-")
+_RGB_BLACK = (0,0,0)
+_RGB_WHITE = (255, 255, 255)
+_GRAPHIC_BAR_X1 = const(220)
+_GRAPHIC_BAR_X2 = const(260)
+_GRAPHIC_BAR_Y1 = const(160)
+_GRAPHIC_BAR_Y2_MIN = _GRAPHIC_BAR_Y1-BidirectionalMotor.MIN_BI_SPEED_PCT
+_GRAPHIC_BAR_Y2_MAX = _GRAPHIC_BAR_Y1-BidirectionalMotor.MAX_BI_SPEED_PCT
 
 class LCD():
     def __init__(self):
-        self._bg_color = st7796.RGB(*RGB_BLACK)
-        self._fg_color = st7796.RGB(*RGB_WHITE)
+        self._bg_color = st7796.RGB(*_RGB_BLACK)
+        self._fg_color = st7796.RGB(*_RGB_WHITE)
         self._last_y2 = 0
 
     def init(self):
@@ -19,10 +20,14 @@ class LCD():
         st7796.Clear(self._bg_color)
 
     def update_motor_speed(self, value: int):
-        st7796.Solid_Box(GRAPHIC_BAR_X1, GRAPHIC_BAR_Y1, GRAPHIC_BAR_X2, self._last_y2, self._bg_color)
-        y2 = GRAPHIC_BAR_Y1-value
-        st7796.Solid_Box(GRAPHIC_BAR_X1, GRAPHIC_BAR_Y1, GRAPHIC_BAR_X2, y2, self._fg_color)
-        self._last_y2 = y2
+        y2 = _GRAPHIC_BAR_Y1-value
+        if value > 0:
+            st7796.Solid_Box(_GRAPHIC_BAR_X1, _GRAPHIC_BAR_Y1, _GRAPHIC_BAR_X2, _GRAPHIC_BAR_Y2_MIN, self._bg_color)
+            st7796.Solid_Box(_GRAPHIC_BAR_X1, y2, _GRAPHIC_BAR_X2, _GRAPHIC_BAR_Y2_MAX, self._bg_color)
+        else:
+            st7796.Solid_Box(_GRAPHIC_BAR_X1, _GRAPHIC_BAR_Y1, _GRAPHIC_BAR_X2, _GRAPHIC_BAR_Y2_MAX, self._bg_color)
+            st7796.Solid_Box(_GRAPHIC_BAR_X1, y2, _GRAPHIC_BAR_X2, _GRAPHIC_BAR_Y2_MIN, self._bg_color)
+        st7796.Solid_Box(_GRAPHIC_BAR_X1, _GRAPHIC_BAR_Y1, _GRAPHIC_BAR_X2, y2, self._fg_color)
 
 if __name__ == "__main__":
     """Test Script"""
