7ace4c0660413a43bcc5797a5862725dad7355b5
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-03 21:46:07 -0500

    fix joystick constants

--- src/hw-5/motor_speed/joystick.py
+++ src/hw-5/motor_speed/joystick.py
@@ -1,12 +1,8 @@
 from machine import ADC, Pin
 from motor import BidirectionalMotor
 
-MAX_U16 = const((2 ** 16) - 1)
-
-MIN_JS_VALUE = const(-100)
-MAX_JS_VALUE = const(100)
-
-JS_FACTOR = const(round((BidirectionalMotor.MAX_SPEED_PCT - BidirectionalMotor.MIN_SPEED_PCT) / MAX_U16))
+_MAX_U16 = const((2 ** 16) - 1)
+_JS_FACTOR = (BidirectionalMotor.MAX_BI_SPEED_PCT - BidirectionalMotor.MIN_BI_SPEED_PCT) / _MAX_U16
 
 class Joystick():
     """Joystick"""
@@ -14,8 +10,8 @@ class Joystick():
         self._adc = ADC(adc_port)
 
     def read(self) -> int:
-        value = (self._adc.read_u16() * JS_FACTOR) + BidirectionalMotor.MIN_SPEED_PCT
-        return (max(min(value, BidirectionalMotor.MAX_SPEED_PCT), BidirectionalMotor.MIN_SPEED_PCT))
+        value = round((self._adc.read_u16() * _JS_FACTOR) + BidirectionalMotor.MIN_BI_SPEED_PCT)
+        return (max(min(value, BidirectionalMotor.MAX_BI_SPEED_PCT), BidirectionalMotor.MIN_BI_SPEED_PCT))
     
 if __name__ == "__main__":
     joystick = Joystick(adc_port=1)
