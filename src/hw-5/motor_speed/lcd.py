f5ff76233f4d4a24dea84956946635e3307a6340
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-03 21:04:27 -0500

    fix bar graph

--- src/hw-5/motor_speed/lcd.py
+++ src/hw-5/motor_speed/lcd.py
@@ -2,7 +2,11 @@ from lib import st7796 #Professor Glower's st7796 driver, renamed for clarity
 
 RGB_BLACK = (0,0,0)
 RGB_WHITE = (255, 255, 255)
-GRAPH_TITLE = const("Motor Speed")
+GRAPHIC_TITLE = const("Motor Speed")
+GRAPHIC_TITLE_COORDS =  (240-8*len(GRAPHIC_TITLE), 5)
+GRAPHIC_BAR_X1 = 220
+GRAPHIC_BAR_X2 = 260
+GRAPHIC_BAR_Y1 = 160
 POS_CHAR = const(" ")
 NEG_CHAR = const("-")
 
@@ -10,15 +14,17 @@ class LCD():
     def __init__(self):
         self._bg_color = st7796.RGB(*RGB_BLACK)
         self._fg_color = st7796.RGB(*RGB_WHITE)
+        self._last_y2 = 0
 
     def init(self):
         st7796.Init()
         st7796.Clear(self._bg_color)
-        st7796.Title(Message=GRAPH_TITLE, Color1=self._fg_color, Color0=self._bg_color)
 
     def update_motor_speed(self, value: int):
-        self.init()
-        st7796.BarChart(X=[value], color1=self._fg_color, color2=self._fg_color)
+        st7796.Solid_Box(GRAPHIC_BAR_X1, GRAPHIC_BAR_Y1, GRAPHIC_BAR_X2, self._last_y2, self._bg_color)
+        y2 = GRAPHIC_BAR_Y1-value
+        st7796.Solid_Box(GRAPHIC_BAR_X1, GRAPHIC_BAR_Y1, GRAPHIC_BAR_X2, y2, self._fg_color)
+        self._last_y2 = y2
 
 if __name__ == "__main__":
     """Test Script"""
--- src/hw-5/motor_speed/main.py
+++ src/hw-5/motor_speed/main.py
@@ -5,8 +5,9 @@ from motor import BidirectionalMotor
 if __name__ == "__main__":
     joystick = Joystick(adc_port=1)
     motor = BidirectionalMotor(cw_gpio=16, ccw_gpio=17, freq_hz=20_000)
-    lcd.init()
+    display = lcd.LCD()
+    display.init()
     while (True):
         js_value = joystick.read()
         motor.set_speed_pct(js_value)
-        lcd.update_motor_speed(js_value)
+        display.update_motor_speed(js_value)
