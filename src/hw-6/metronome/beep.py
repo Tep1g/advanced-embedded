8f6004ab2ad262a5597adbb499784884a5b8e06e
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-13 17:29:59 -0500

    measure time between beeps in test script

--- src/hw-6/metronome/beep.py
+++ src/hw-6/metronome/beep.py
@@ -1,4 +1,5 @@
 from machine import Pin, PWM, Timer
+from time import ticks_us
 
 _DUTY_ON_U16 = round(((2 ** 16) - 1) / 2)
 _DUTY_OFF_U16 = const(0)
@@ -20,7 +21,17 @@ class Beeper:
         timer.init(mode=Timer.ONE_SHOT, period=self.quiet_period_ms, callback=self._start_beep_handler)
 
 if __name__ == "__main__":
-    """Test Script"""
+    """Rough test script for measuring quiet period between beeps"""
     beeper = Beeper(quiet_period_ms=1000, beep_length_ms=10, beep_freq=500, beep_gpio=28)
+    got_start_ticks = False
+    got_stop_ticks = False
     while (True):
-        continue
+        if ((beeper._beeper.duty_u16() == 0) and (not got_start_ticks)):
+            start_ticks_us = ticks_us()
+            got_start_ticks = True
+            got_stop_ticks = False
+
+        elif ((beeper._beeper.duty_u16() != 0) and (not got_stop_ticks)):
+            print(ticks_us()-start_ticks_us)
+            got_stop_ticks = True
+            got_start_ticks = False
