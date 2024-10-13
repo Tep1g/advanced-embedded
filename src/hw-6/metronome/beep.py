2919f7326098566c7c0deea1e519557ab661c078
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-13 02:45:24 -0500

    switch to u16 duty cycle

--- src/hw-6/metronome/beep.py
+++ src/hw-6/metronome/beep.py
@@ -1,22 +1,22 @@
 from machine import Pin, PWM, Timer
 
-_DUTY_ON_NS = const(500_000_000)
-_DUTY_OFF_NS = const(0)
+_DUTY_ON_U16 = round(((2 ** 16) - 1) / 2)
+_DUTY_OFF_U16 = const(0)
 
 class Beeper:
     def __init__(self, between_beeps_ms: int, beep_length_ms: int, beep_freq: int, beep_gpio: int):
         self.between_beeps_ms = between_beeps_ms
         self._beep_length_ms = beep_length_ms
-        self._beeper = PWM(dest=Pin(beep_gpio, Pin.OUT), freq=beep_freq, duty_ns=_DUTY_OFF_NS)
+        self._beeper = PWM(Pin(beep_gpio, Pin.OUT), freq=beep_freq, duty_u16=_DUTY_OFF_U16)
         self._timer = Timer(-1)
         self._timer.init(mode=Timer.ONE_SHOT, period=self.between_beeps_ms, callback=self._start_beep_handler)
 
     def _start_beep_handler(self, timer: Timer):
-        self._beeper.duty_ns(_DUTY_ON_NS)
+        self._beeper.duty_u16(_DUTY_ON_U16)
         timer.init(mode=Timer.ONE_SHOT, period=self._beep_length_ms, callback=self._stop_beep_handler)
 
     def _stop_beep_handler(self, timer: Timer):
-        self._beeper.duty_ns(_DUTY_OFF_NS)
+        self._beeper.duty_u16(_DUTY_OFF_U16)
         timer.init(mode=Timer.ONE_SHOT, period=self.between_beeps_ms, callback=self._start_beep_handler)
 
 if __name__ == "__main__":
