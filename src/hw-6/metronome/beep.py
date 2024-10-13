22a1cb344e978fd5f798b8a63f6a370d39b014bc
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-13 17:30:26 -0500

    rename _pwm variable

--- src/hw-6/metronome/beep.py
+++ src/hw-6/metronome/beep.py
@@ -8,16 +8,16 @@ class Beeper:
     def __init__(self, quiet_period_ms: int, beep_length_ms: int, beep_freq: int, beep_gpio: int):
         self.quiet_period_ms = quiet_period_ms
         self._beep_length_ms = beep_length_ms
-        self._beeper = PWM(Pin(beep_gpio, Pin.OUT), freq=beep_freq, duty_u16=_DUTY_OFF_U16)
+        self._pwm = PWM(Pin(beep_gpio, Pin.OUT), freq=beep_freq, duty_u16=_DUTY_OFF_U16)
         self._timer = Timer(-1)
         self._timer.init(mode=Timer.ONE_SHOT, period=self.quiet_period_ms, callback=self._start_beep_handler)
 
     def _start_beep_handler(self, timer: Timer):
-        self._beeper.duty_u16(_DUTY_ON_U16)
+        self._pwm.duty_u16(_DUTY_ON_U16)
         timer.init(mode=Timer.ONE_SHOT, period=self._beep_length_ms, callback=self._stop_beep_handler)
 
     def _stop_beep_handler(self, timer: Timer):
-        self._beeper.duty_u16(_DUTY_OFF_U16)
+        self._pwm.duty_u16(_DUTY_OFF_U16)
         timer.init(mode=Timer.ONE_SHOT, period=self.quiet_period_ms, callback=self._start_beep_handler)
 
 if __name__ == "__main__":
@@ -26,12 +26,12 @@ if __name__ == "__main__":
     got_start_ticks = False
     got_stop_ticks = False
     while (True):
-        if ((beeper._beeper.duty_u16() == 0) and (not got_start_ticks)):
+        if ((beeper._pwm.duty_u16() == 0) and (not got_start_ticks)):
             start_ticks_us = ticks_us()
             got_start_ticks = True
             got_stop_ticks = False
 
-        elif ((beeper._beeper.duty_u16() != 0) and (not got_stop_ticks)):
+        elif ((beeper._pwm.duty_u16() != 0) and (not got_stop_ticks)):
             print(ticks_us()-start_ticks_us)
             got_stop_ticks = True
             got_start_ticks = False
