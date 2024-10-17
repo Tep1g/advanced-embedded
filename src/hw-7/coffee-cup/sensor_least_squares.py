b2fa70034a9eeb088b3df5c49befd711ecd63c5e
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-17 13:52:01 -0500

    remove redundant class members

--- src/hw-7/coffee-cup/sensor_least_squares.py
+++ src/hw-7/coffee-cup/sensor_least_squares.py
@@ -5,17 +5,15 @@ from machine import Timer
 from sensor import Sensor
 
 class SensorLeastSquares:
-    def __init__(self, onewire_gpio: int, sample_period_ms: int, num_samples: int, Tamb_c: float):
+    def __init__(self, onewire_gpio: int, sample_period_ms: int, num_samples: int, t_amb_c: float):
         self._sensor = Sensor(onewire_gpio=onewire_gpio)
         self._sample_period_s = sample_period_ms/1000
-        self._Tamb_c = Tamb_c
+        self._t_amb_c = t_amb_c
         self._B =  [[0.01,0],[0,0.01]]
         self._Y = [[0],[0]]
         self._x = 0
         self._timer = Timer(-1)
         self._timer.init(mode=Timer.PERIODIC, period=sample_period_ms, callback=self._sensor_handler)
-        self._samples_deg_c = []
-        self._sample_index = 0
         
     def _sensor_handler(self, timer: Timer):
         self._sensor._ds18b20.convert_temp()
@@ -23,7 +21,7 @@ class SensorLeastSquares:
         temp_c = self._sensor._ds18b20.read_temp()
 
         self._x += self._sample_period_s
-        y = math.log(temp_c - self._Tamb_c)
+        y = math.log(temp_c - self._t_amb_c)
 
         matrix.add(self._B, [[self._x*self._x, self._x], [self._x, 1]])
         matrix.add(self._Y, [[self._x*y], [y]])
