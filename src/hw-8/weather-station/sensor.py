c9fa012749c7c3ad70681b66d2058775286858ba
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-10-26 20:06:27 -0500

    update weather-station
    Removed circular buffer, print and plot data to terminal, plot depends on the button

--- src/hw-8/weather-station/sensor.py
+++ src/hw-8/weather-station/sensor.py
@@ -3,40 +3,52 @@ from machine import I2C, Pin, Timer
 from bme280 import BME280
 
 class EnvironmentSensor:
-    def __init__(self, device_id: int, scl_gpio: int, sda_gpio: int, scl_freq_hz: int, sample_rate_hz: int, buffer_size: int, button=None):
+    def __init__(self, device_id: int, scl_gpio: int, sda_gpio: int, scl_freq_hz: int, sample_rate_hz: int, duration: int, print_data=False, graph_data=False, button=None):
         self._i2c = I2C(id=device_id, scl=Pin(scl_gpio), sda=Pin(sda_gpio), freq=scl_freq_hz)
         self._bme = BME280(i2c=self._i2c)
-        self._circ_buffer = [[""] * buffer_size]*3
+        self._data = [[],[],[]]
+        self._data_set_ptr = 0
+        self._done_collecting = False
+        self._graph_data = graph_data
+        self._time_s = 0
+        self._print_data = print_data
+        self._duration_s = duration
         self._timer = Timer(-1)
-        self._buffer_ptr = 0
-        self._set_num = 1
-        self._set_label = "Set {}".format(self._set_num)
-        self._current_set = {}
-        self._meas_sets = {}
-        lcd.init()
         self._timer.init(mode=Timer.PERIODIC, period=sample_rate_hz, callback=self._sample_handler)
-        if button != None:
+        if (button != None) and (self._graph_data):
             self._button = button
             self._button.irq(trigger=Pin.IRQ_FALLING, handler=self._switch_set_handler)
 
     def _sample_handler(self, timer: Timer):
+        self._time_s += 1
+
         temperature = self._bme.temperature
         humidity = self._bme.humidity
         pressure = self._bme.pressure
 
-        self._circ_buffer[0][self._buffer_ptr] = temperature
-        self._circ_buffer[1][self._buffer_ptr] = humidity
-        self._circ_buffer[2][self._buffer_ptr] = pressure
-        self._buffer_ptr += 1
+        self._data[0].append(temperature)
+        self._data[1].append(humidity)
+        self._data[2].append(pressure)
 
-        lcd.update(temperature=temperature, humidity=humidity, pressure=pressure)
+        if self._time_s >= self._duration_s:
+            self._timer.deinit()
+            self._done_collecting = True
 
-    
+            if self._print_data:
+                for i in range(len(self._data)):
+                    print("T: {}, Temp: {}, Humid: {}, Press: {}".format(
+                        i, 
+                        self._data[0][i], 
+                        self._data[1][i], 
+                        self._data[2][i]
+                        )
+                    )
+
+            if self._graph_data:
+                lcd.plot(self._data[self._data_set_ptr])
+
+
     def _switch_set_handler(self, pin: Pin):
-        self._meas_sets[self._set_label, {
-            "temperature": [temp for temp in self._circ_buffer[0]],
-            "humidity": [humid for humid in self._circ_buffer[1]],
-            "pressure": [pres for pres in self._circ_buffer[2]]
-            }
-        ]
-        self._set_label = "Set {}".format(self._set_num)
+        if self._done_collecting:
+            self._data_set_ptr = (self._data_set_ptr + 1) % len(self._data)
+            lcd.plot(self._data[self._data_set_ptr])
