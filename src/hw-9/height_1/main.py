17cdd85c2dd5aafa992b06c15d6958cb385e8400
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-11-07 18:05:05 -0600

    display top times on LCD

--- src/hw-9/height_1/main.py
+++ src/hw-9/height_1/main.py
@@ -1,15 +1,26 @@
+import st7796
 from imu import MPU6050
 from machine import I2C, Pin
 from time import ticks_ms
 
 G_THRESHOLD = 0.1
+RGB_BLACK = st7796.RGB(0,0,0)
+RGB_WHITE = st7796.RGB(255,255,255)
 
 if __name__ == "__main__":
+    st7796.Init()
+    st7796.Clear(RGB_BLACK)
     i2c = I2C(device_id=0, scl=Pin(1), sda=Pin(0))
     gy521 = MPU6050(i2c)
     button = Pin(15, Pin.IN, Pin.PULL_UP)
     top_times = [0, 0, 0]
     while True:
+        for i in range(len(top_times)):
+            time_str = str(top_times[i])
+            clear_string = " " * len(time_str)
+            st7796.Text2(clear_string, 0, 0-(50*i), RGB_WHITE, RGB_BLACK)
+            st7796.Text2(time_str, 0, 0-(50*i), RGB_WHITE, RGB_BLACK)
+
         while button.value() == 1:
             while not (0 < gy521.accel.z < G_THRESHOLD):
                 continue
