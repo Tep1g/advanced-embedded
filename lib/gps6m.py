4e87d4fc4a881fcb357ab36fb5cf56b36d708873
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-11-16 15:59:03 -0600

    handle invalid string position in gps reading

--- lib/gps6m.py
+++ lib/gps6m.py
@@ -7,9 +7,31 @@ class GPS6M:
         self._uart = uart
 
     def read_speed_m_per_s(self):
-        msg = self._uart.readline()
-        if msg != None:
-            speed_knots_str = msg[46:51]
-            return float(speed_knots_str) * _KNOTS_TO_M_PER_S
-        else:
-            raise TimeoutError("No gps msg received")
+        msg = self._read_line()
+        speed_knots_str = msg[46:51]
+
+        # simpler error handling in case of invalid cast
+        try:
+            speed_knots = float(speed_knots_str)
+        except:
+            return 0.0
+        
+        return speed_knots * _KNOTS_TO_M_PER_S
+
+    def _read_line(self):
+        flag = 0
+        msg = ''
+        while(flag == 0):
+            x = self._uart.read(1)
+            if(x != None):
+                x = ord(x)
+            else:
+                raise TimeoutError("No gps msg received")
+
+            if(chr(x) == '$'):
+                msg = ''
+            if(x == 13):
+                flag = 1
+            else:
+                msg = msg + chr(x)
+        return(msg)
