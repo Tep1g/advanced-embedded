1359762e66583c53f11190ecc3e64a2513b72dfd
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-11-22 02:35:43 -0600

    condense RGBController to one button irq

--- src/project/main.py
+++ src/project/main.py
@@ -5,13 +5,12 @@ from rgb import RGBController
 
 if __name__ == "__main__":
     neopixel = NeoPixel(Pin(11), 8, bpp=3, timing=1)
-    rgb_switch = Pin(15, Pin.IN, Pin.PULL_UP)
-    select_button = Pin(14, Pin.IN, Pin.PULL_UP)
+    button = Pin(14, Pin.IN, Pin.PULL_UP)
     lcd = LCD(stick_x_gpio=26, stick_y_gpio=27)
-    strip = RGBController(rgb_switch, select_button, neopixel, lcd)
+    strip = RGBController(button, neopixel, lcd)
     lcd.init()
     while True:
         try:
             lcd.tick()
         except KeyboardInterrupt:
-            break  
+            break
--- src/project/rgb.py
+++ src/project/rgb.py
@@ -3,19 +3,22 @@ from machine import Pin
 from neopixel import NeoPixel
 
 class RGBController:
-    def __init__(self, rgb_switch: Pin, select_button: Pin, neopixel: NeoPixel, lcd: LCD):
+    def __init__(self, button: Pin, neopixel: NeoPixel, lcd: LCD):
         self._neopixel = neopixel
         self._lcd = lcd
         self._rgb_pointer = 0
         self._np_selected = False
         self._current_rgb = [0, 0, 0]
-        self._rgb_switch = rgb_switch
-        self._select_button = select_button
-        self._rgb_switch.irq(handler=self._rgb_switch_handler, trigger=Pin.IRQ_FALLING)
-        self._select_button.irq(handler=self._select_handler, trigger=Pin.IRQ_FALLING)
+        self._button = button
+        self._button.irq(handler=self._button_handler, trigger=Pin.IRQ_FALLING)
 
-    def _rgb_switch_handler(self, pin: Pin):
-        if self._np_selected:
+    def _button_handler(self, pin: Pin):
+        if not self._np_selected:
+            self._lcd.switch_box_rgb(self._rgb_pointer)
+            self._np_selected = True
+            self._lcd.vertically_constrained = True
+            print("Selected NeoPixel {}".format(self._lcd.box_pos[1]))
+        else:
             self._current_rgb[self._rgb_pointer] = 31 * self._lcd.box_pos[0]
             self._neopixel[self._lcd.box_pos[1]] = tuple(self._current_rgb) #type: ignore
             self._neopixel.write()
@@ -28,10 +31,3 @@ class RGBController:
                 self._current_rgb = [0, 0, 0]
             else:
                 self._lcd.switch_box_rgb(self._rgb_pointer)
-
-    def _select_handler(self, pin: Pin):
-        if not self._np_selected:
-            self._lcd.switch_box_rgb(self._rgb_pointer)
-            self._np_selected = True
-            self._lcd.vertically_constrained = True
-            print("Selected NeoPixel {}".format(self._lcd.box_pos[1]))
