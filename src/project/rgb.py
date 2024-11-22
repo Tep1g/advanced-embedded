9373877638a9e841d380c871686638bede720682
Tep1g <66882646+Tep1g@users.noreply.github.com>
2024-11-22 02:44:49 -0600

    add print statements for setting RGB values

--- src/project/rgb.py
+++ src/project/rgb.py
@@ -2,6 +2,8 @@ from lcd import LCD
 from machine import Pin
 from neopixel import NeoPixel
 
+RGB_STRING = "RGB"
+
 class RGBController:
     def __init__(self, button: Pin, neopixel: NeoPixel, lcd: LCD):
         self._neopixel = neopixel
@@ -22,11 +24,13 @@ class RGBController:
             self._current_rgb[self._rgb_pointer] = 31 * self._lcd.box_pos[0]
             self._neopixel[self._lcd.box_pos[1]] = tuple(self._current_rgb) #type: ignore
             self._neopixel.write()
+            print("Set {} value to {}".format(RGB_STRING[self._rgb_pointer], self._current_rgb[self._rgb_pointer]))
             self._rgb_pointer = (self._rgb_pointer + 1) % 3
 
             if self._rgb_pointer == 0:
                 self._np_selected = False
                 self._lcd.vertically_constrained = False
+                print("Set NeoPixel {} to RGB value {}".format(self._lcd.box_pos[1], self._current_rgb))
                 self._lcd.clear_box_rgb()
                 self._current_rgb = [0, 0, 0]
             else:
